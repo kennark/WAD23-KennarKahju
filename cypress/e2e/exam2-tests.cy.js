a8db8e13c226363685cd9c62cf42f89d5cd473e0
Kennar Kahju <68914924+kennark@users.noreply.github.com>
2024-01-19 16:08:26 +0200

    task 7

--- cypress/e2e/exam2-tests.cy.js
+++ cypress/e2e/exam2-tests.cy.js
@@ -1,6 +1,11 @@
 /// <reference types = "cypress"/>  
 
 it('Test 1', () => {
-
-    
+    cy.visit("http://localhost:8080/Instructor");
+    cy.get("h1").should("contain", "Instructor Management Pannel");
+    cy.wait(500);
+    cy.get("input[name='exam']:first").clear().type("0");
+    cy.get("button[class='update']:first").click();
+    cy.wait(500);
+    cy.get("td:last").click();
 })
